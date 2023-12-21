43c84a601487ed6641c21cb73a15d6b8b523d1ce
Sheharyar <sheharyar.tariq44@gmail.com>
2023-12-22 00:55:40 +0500

    add detail template and vote() function in views

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404 , HttpResponseRedirect
+from django.urls import reverse
 
 
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -18,4 +19,14 @@ def results(request, question_id):
     return HttpResponse("You're looking at results of question %s." % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting question %s." % question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+         selected_choice = question.choice_set.get(pk = request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/details.html', {'question': question, 'error_message' : "you didn't select a choice"})
+    else:
+        selected_choice.vote += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args = (question.id)))
+
+
