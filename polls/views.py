7855dfddad24464039d82ea6766e0ededdce9a81
Sheharyar <sheharyar.tariq44@gmail.com>
2023-12-22 22:36:57 +0500

    add voting form, results template and generic views

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404 , HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 
 from .models import Question, Choice
@@ -16,17 +17,34 @@ def detail(request, question_id):
     return render(request, 'polls/details.html', {"question" : question})
 
 def results(request, question_id):
-    return HttpResponse("You're looking at results of question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question':question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk = question_id)
     try:
          selected_choice = question.choice_set.get(pk = request.POST['choice'])
     except(KeyError, Choice.DoesNotExist):
-        return render(request, 'polls/details.html', {'question': question, 'error_message' : "you didn't select a choice"})
+        return render(request, 'polls/details.html', {'question': question, 'error_message' : "you didn't select a choice.",},)
     else:
-        selected_choice.vote += 1
+        selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', args = (question.id)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name  = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/details.html"
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+    
 
 
